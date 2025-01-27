5d4edeaae42dac4bc4819c0e94565852734da23f
unknown <Lenovo@WFH104-NIF0924314.NIFTEL.LOCAL>
2025-01-27 17:25:31 +0530

    Day 1 of Alternate days of this project

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Indulge in a diverse menu of mouthwatering dishes, carefully crafted with 
+                the finest ingredients and a touch of culinary artistry. Our goal is simple
+                : to delight your taste buds and make every meal an unforgettable experience.
+                Come savor the flavors that turn cravings into pure satisfaction!.</p>
+            <button>View Menu</button>    
+        </div>
+    </div>
+  )
+}
+
+export default Header
